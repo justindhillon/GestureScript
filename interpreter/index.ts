4f8771378075c0b64dfbde1d93869ce7f5640fd0
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-03-01 05:13:56 -0800

    add interpreter

--- interpreter/index.ts
+++ interpreter/index.ts
@@ -1,17 +1,88 @@
-const TOKEN_PLUS = '1'
-const TOKEN_MINUS = '2'
-const TOKEN_PREVIOUS = '3'
-const TOKEN_NEXT = '4'
-const TOKEN_LOOP_START = '5'
-const TOKEN_LOOP_END = '6'
-const TOKEN_OUTPUT = '7'
-const TOKEN_INPUT = '8'
+// How large memory blocks are (size of array)
+const MEMORY_SIZE = 10000;
+
+const TOKEN_PLUS = '1';
+const TOKEN_MINUS = '2';
+const TOKEN_PREVIOUS = '3';
+const TOKEN_NEXT = '4';
+const TOKEN_LOOP_START = '5';
+const TOKEN_LOOP_END = '6';
+const TOKEN_OUTPUT = '7';
 
 // 1, 2 is ✋
 // 3, 4 is 🤟
 // 5, 6 is 👈
-// 7, 8 is 👌
+// 7 is 👌
 
-export async function interpreter(signs: number) {
-    console.log("test");
+// Hello World!
+// 1111111154111154114111411141333326414142441536326447422271111111771117447327371117222222722222222744174117
+
+export function interpreter(signs: string): string | null {
+    const memory = new Array(MEMORY_SIZE).fill(0);
+    let signPointer = 0;
+    let memoryPointer = 0;
+    let addressStack = [];
+
+    let output = "";
+    let terminate = false;
+
+    while (!terminate) {
+        switch (signs[signPointer]) {
+            case TOKEN_PLUS:
+                memory[memoryPointer]++;
+                break;
+            case TOKEN_MINUS:
+                memory[memoryPointer]--;
+                break;
+            case TOKEN_PREVIOUS:
+                if (0 < memoryPointer)
+                    memoryPointer--;
+                break;
+            case TOKEN_NEXT:
+                if (memoryPointer === memory.length - 1) 
+                    // Out of memory, allocate another block
+                    memory.push(new Array(MEMORY_SIZE).fill(0));
+                memoryPointer++;
+                break;
+            case TOKEN_LOOP_START:
+                if (memory[memoryPointer]) {
+                    addressStack.push(signPointer);
+                } else {
+                    // Goto TOKEN_LOOP_END
+                    let nestedLoops = 0;
+                    while (true) {
+                        signPointer++;
+                        if (!signs[signPointer]) 
+                            // The program has been executed
+                            break;
+                        if (signs[signPointer] === TOKEN_LOOP_START) {
+                            nestedLoops++;
+                        } else if (signs[signPointer] === TOKEN_LOOP_END) {
+                            if (nestedLoops) {
+                                nestedLoops--;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+                break;
+            case TOKEN_LOOP_END:
+                signPointer = addressStack.pop() - 1;
+                break;
+            case TOKEN_OUTPUT:
+                output += String.fromCharCode(memory[memoryPointer]);
+                break;
+            case undefined:
+                // The program has been executed
+                terminate = true;
+                break;
+            default:
+                console.error("Error: signs includes invalid value", signs[signPointer]);
+                return null;
+        }
+        signPointer++; 
+    }
+
+    return output;
 }
