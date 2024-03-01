0cebe496383c9a1e8daeb398e1c2247449c14d0d
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-02-29 17:13:24 -0800

    add hello world input

---
+++ interpreter/index.ts
@@ -0,0 +1,17 @@
+const TOKEN_PLUS = '1'
+const TOKEN_MINUS = '2'
+const TOKEN_PREVIOUS = '3'
+const TOKEN_NEXT = '4'
+const TOKEN_LOOP_START = '5'
+const TOKEN_LOOP_END = '6'
+const TOKEN_OUTPUT = '7'
+const TOKEN_INPUT = '8'
+
+// 1, 2 is ✋
+// 3, 4 is 🤟
+// 5, 6 is 👈
+// 7, 8 is 👌
+
+export async function interpreter(signs: number) {
+    console.log("test");
+}
