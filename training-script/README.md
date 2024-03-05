4282103d6442bf38bb7b33a560bbba60b37a8111
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-03-05 15:02:39 -0800

    add documentation to training-script

---
+++ training-script/README.md
@@ -0,0 +1,37 @@
+# How to make your own AI model
+This directory has everything you need to make the AI model.
+
+## Install Dependencies
+Run this command to install dependencies
+```bash
+pip3 install pyqt5 lxml
+```
+
+## Getting Images
+Run this script to use your webcam to capture training images.
+```bash
+python main.py
+```
+The files are in ```training-images```
+
+## Seting Up labelImg
+
+```bash
+cd labelImg
+
+# Install pyqt5 on Ubuntu
+sudo apt-get install pyqt5-dev-tools
+
+# Install pyqt5 on Fedora
+sudo dnf install PyQt5
+
+# Build and runing labelImg.py
+make qt5py3
+python3 labelImg.py
+```
+
+## Using labelImg
+1. ```Open Dir``` training-images
+2. ```Change Save Dir``` training-images
+3. Set ```Auto Save Mode``` in ```view```
+4. Use ```Create RectBox``` on all the training-images
--- training-script/main.py
+++ training-script/main.py
@@ -1,18 +1,15 @@
-# Run this and then run labelImg on training-images
-
 import cv2
 import os
 import time
 import uuid
 
-IMAGES_PATH = '../training-images'
+IMAGES_PATH = './training-images'
 labels = ['✋', '✋-left', '🤟', '🤟-left', '👈', '👈-left', '👌']
 number_imgs = 15
 
 if __name__ == "__main__":
     for label in labels:
-        directory_path = os.path.join(IMAGES_PATH, label)
-        os.makedirs(directory_path, exist_ok=True)
+        os.makedirs(IMAGES_PATH, exist_ok=True)
 
         video = cv2.VideoCapture(0)
         print(f"Capture images for: {label}")
@@ -20,7 +17,7 @@ if __name__ == "__main__":
 
         for i in range(number_imgs):
             ret, frame = video.read()
-            image_name = os.path.join(directory_path, f"{label}_{str(uuid.uuid1())}.jpg")
+            image_name = os.path.join(IMAGES_PATH, f"{label}_{str(uuid.uuid1())}.jpg")
             cv2.imwrite(image_name, frame)
             cv2.imshow('Capturing Images', frame)
             time.sleep(1)
